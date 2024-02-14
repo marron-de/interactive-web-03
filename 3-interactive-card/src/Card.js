fbfff8c7d22e9b87b7170ce369636cb45d338cf5
수정-PC\z8545 <jeong8545@gmail.com>
2024-02-15 01:09:24 +0900

    init

--- 3-interactive-card/src/Card.js
+++ 3-interactive-card/src/Card.js
@@ -1,42 +1,42 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 // const card = new Card({
 //   width: 2,
 //   height: 3,
-//   color: '#0077ff',
+//   color: '#0077ff'
 // })
 
 class Card {
   constructor({ width, height, radius, color }) {
-    const x = width / 2 - radius;
-    const y = height / 2 - radius;
-
+    const x = width/2 - radius // Three.js은 원점 중심으로 x, y 계산
+    const y = height/2 - radius // Three.js은 원점 중심으로 x, y 계산
     const shape = new THREE.Shape();
 
-    shape
-      .absarc(x, y, radius, Math.PI / 2, 0, true)
-      .lineTo(x + radius, -y)
-      .absarc(x, -y, radius, 0, -Math.PI / 2, true)
-      .lineTo(-x, -(y + radius))
-      .absarc(-x, -y, radius, -Math.PI / 2, Math.PI, true)
-      .lineTo(-(x + radius), y, radius, Math.PI, Math.PI / 2, true)
-      .absarc(-x, y, radius, Math.PI, Math.PI / 2, true);
+    shape // 카드형태
+    .absarc(x, y, radius, Math.PI / 2, 0, true) // 1번째 곡선
+    .lineTo(x + radius, -y) // 2번째 점까지 선분 
+    .absarc(x, -y, radius, 0, -Math.PI / 2, true) // 2번째 곡선
+    .lineTo(-x, -(y + radius)) // 3번째 점까지 선분 
+    .absarc(-x, -y, radius, -Math.PI / 2, Math.PI, true) // 3번째 곡선
+    .lineTo(-(x + radius), y) // 4번째 점까지 선분 
+    .absarc(-x, y, radius, Math.PI, Math.PI / 2, true) // 4번째 곡선
 
-    const geometry = new THREE.ExtrudeGeometry(shape, {
+    const geometry = new THREE.ExtrudeGeometry(shape, { // 두께 표현 Geometry
       depth: 0.01,
       bevelThickness: 0.1,
-    });
-    const material = new THREE.MeshStandardMaterial({
-      color,
-      side: THREE.DoubleSide,
-      roughness: 0.5,
-      metalness: 0.5,
-    });
+    }); 
 
-    const mesh = new THREE.Mesh(geometry, material);
+    const material = new THREE.MeshStandardMaterial({ 
+      color,
+      side: THREE.DoubleSide, // 성능적인 이유로 기본값은 앞면만 노출됨, 추가 설정필요
+      roughness: 0.3,
+      metalness: 0.5,
+     })
+
+    const mesh = new THREE.Mesh(geometry, material)
 
     this.mesh = mesh;
   }
 }
 
-export default Card;
+export default Card
--- 3-interactive-card/src/main.js
+++ 3-interactive-card/src/main.js
@@ -1,126 +1,128 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { gsap } from 'gsap';
-import { GUI } from 'lil-gui';
-import Card from './Card';
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { gsap } from 'gsap'
+import { GUI } from 'lil-gui'
+import Card from "./Card";
 
-window.addEventListener('load', function () {
+window.addEventListener("load", () => {
   init();
 });
 
 function init() {
-  const gui = new GUI();
+  const gui = new GUI()
 
-  const COLORS = ['#ff6e6e', '#31e0c1', '#006fff', '#ffd732'];
+  const COLORS = ['#ff6e6e', '#31e0c1', '#006fff', "#ffd732"]
 
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
-    alpha: true,
+    alpha: true, // 투명 처리
   });
 
+  // renderer.setClearAlpha(0.5);
+  // renderer.setClearColor(0x00aaff, 0.5) // 배경과 투명도 같이 설정
   renderer.setSize(window.innerWidth, window.innerHeight);
-
   document.body.appendChild(renderer.domElement);
 
   const scene = new THREE.Scene();
+  // const textureLoader = new THREE.TextureLoader();
+  // const texture = textureLoader.load('./assets/sky_texture.jpg')
+  // scene.background = texture;
 
   const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    500,
-  );
+    75, 
+    window.innerWidth / window.innerHeight, 
+    1, 
+    500,    
+  ); 
+  camera.position.z = 25; 
 
-  camera.position.z = 25;
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-
+  /** Controls */
+  const controls = new OrbitControls(camera, renderer.domElement)
   controls.autoRotate = true;
   controls.autoRotateSpeed = 2.5;
-  controls.rotateSpeed = 0.75;
-  controls.enableDamping = true;
+  controls.rotateSpeed = 0.75; // 드래그 시 회전속도
+  controls.enableDamping = true; // 관성
   controls.enableZoom = false;
   controls.minPolarAngle = Math.PI / 2 - Math.PI / 3;
   controls.maxPolarAngle = Math.PI / 2 + Math.PI / 3;
+ 
 
+  /** Card */
   const card = new Card({
     width: 10,
     height: 15.8,
     radius: 0.5,
     color: COLORS[0],
   });
-
   card.mesh.rotation.z = Math.PI * 0.1;
+  scene.add(card.mesh)
 
-  scene.add(card.mesh);
+  gsap.to(card.mesh.rotation, { y: -Math.PI * 4, duration: 2.5, ease: 'back.out(2.5)' })
 
-  gsap.to(card.mesh.rotation, { y: -Math.PI * 4, duration: 2.5, ease: 'back.out(2.5)' });
+  const cardFoler = gui.addFolder('Card')
 
-  const cardFolder = gui.addFolder('Card');
+  cardFoler
+  .add(card.mesh.material, 'roughness')
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name('material.roughness');
 
-  cardFolder
-    .add(card.mesh.material, 'roughness')
-    .min(0)
-    .max(1)
-    .step(0.01)
-    .name('material.roughness');
+  cardFoler
+  .add(card.mesh.material, 'metalness')
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name('material.metalness');
 
-  cardFolder
-    .add(card.mesh.material, 'metalness')
-    .min(0)
-    .max(1)
-    .step(0.01)
-    .name('material.metalness');
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+  /** AmbientLight */ // 색표현을 위해 조명 추가
+ const ambientLight = new THREE.AmbientLight(0xffffff, 2)
+ ambientLight.position.set(-5, -5, -5);
+ scene.add(ambientLight)
 
-  ambientLight.position.set(-5, -5, -5);
 
-  scene.add(ambientLight);
+ /** DirectionalLight */
+ const directionalLight1 = new THREE.DirectionalLight(0xffffff, 0.6);
+ const directionalLight2 = directionalLight1.clone();
+ directionalLight1.position.set(1, 1, 3);
+ directionalLight2.position.set(-1, 1, -3);
+ scene.add(directionalLight1, directionalLight2)
 
-  const directionalLight1 = new THREE.DirectionalLight(0xffffff, 0.6);
-  const directionalLight2 = directionalLight1.clone();
-
-  directionalLight1.position.set(1, 1, 3);
-  directionalLight2.position.set(-1, 1, -3);
-
-  scene.add(directionalLight1, directionalLight2);
 
   render();
 
   function render() {
     controls.update();
-
     renderer.render(scene, camera);
-
     requestAnimationFrame(render);
   }
 
   function handleResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
-
-    camera.updateProjectionMatrix();
+    camera.updateProjectionMatrix(); // 이 코드를 입력해줘야 반영됨
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     renderer.render(scene, camera);
   }
 
-  window.addEventListener('resize', handleResize);
+  window.addEventListener("resize", handleResize);
 
-  const container = document.querySelector('.container');
 
+  /** Button */
+  const container = document.querySelector(".container");
   COLORS.forEach(color => {
-    const button = document.createElement('button');
+    const button = document.createElement('button')
 
     button.style.backgroundColor = color;
-
+    
     button.addEventListener('click', () => {
       card.mesh.material.color = new THREE.Color(color);
 
-      gsap.to(card.mesh.rotation, { y: card.mesh.rotation.y - Math.PI / 2, duration: 1, ease: 'back.out(2.5)' });
+      gsap.to(card.mesh.rotation, { y: card.mesh.rotation.y - Math.PI / 2, duration: 1, ease: 'back.out(2.5)' })
     })
 
-    container.appendChild(button);
-  });
+    container.appendChild(button)
+  })
 }
